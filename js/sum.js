c20d9ce066091337db243b78cbf7f5e1e55822bc
phat <nvtpk19hutech@gmail.com>
2023-02-15 17:48:08 +0700

    Added my project

---
+++ js/sum.js
@@ -0,0 +1,30 @@
+setTimeout(() => {
+  console.log(1);
+  setTimeout(() => {
+    console.log(2);
+    setTimeout(() => {
+      console.log(3);
+      setTimeout(() => {
+        console.log(4);
+        setTimeout(() => {
+          console.log(5);
+          setTimeout(() => {
+            console.log(6);
+            setTimeout(() => {
+              console.log(7);
+              setTimeout(() => {
+                console.log(8);
+                setTimeout(() => {
+                  console.log(9);
+                  setTimeout(() => {
+                    console.log(10);
+                  }, 1500);
+                }, 1500);
+              }, 1500);
+            }, 1500);
+          }, 1500);
+        }, 1500);
+      }, 1500);
+    }, 1500);
+  }, 1500);
+}, 1500);
